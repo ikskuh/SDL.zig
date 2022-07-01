e570b1e792447d713a56a280cb42a6d90449d616
Sucuturdean David <79461198+Sucuturdean@users.noreply.github.com>
2022-07-01 19:38:30 +0300

    Added basic bindings

--- src/wrapper/ttf.zig
+++ src/wrapper/ttf.zig
@@ -6,12 +6,86 @@ pub fn init() !void {
         return makeError();
 }
 
+pub fn wasInit()
+
 pub fn quit() void {
     sdl.c.TTF_Quit();
 }
 
 pub const Font = struct {
     ptr: *sdl.c.TTF_Font,
+
+    pub const Style = struct {
+        italic: bool = false,
+        bold: bool = false,
+        underline: bool = false,
+
+        pub const normal = Style{};
+    };
+
+    pub fn getStyle(self: Font) Style {
+        const value = sdl.c.TTF_GetFontStyle(self.ptr);
+        var result = Style.normal;
+        if(value & sdl.c.TTF_STYLE_ITALIC) result.italic = true;
+        if(value & sdl.c.TTF_STYLE_BOLD) result.bold = true;
+        if(value & sdl.c.TTF_STYLE_UNDERLINE) result.underline = true;
+        return result; 
+    }
+
+    pub fn setStyle(self: Font, style: Style) void {
+        var value: c_int = 0;
+        if(style.italic) value |= sdl.c.TTF_STYLE_ITALIC;
+        if(style.bold) value |= sdl.c.TTF_STYLE_BOLD;
+        if(style.underline) value |= sdl.c.TTF_STYLE_UNDERLINE;
+        sdl.c.TTF_SetFontStyle(self.ptr, value);
+    }
+
+    pub fn height(self: Font) c_int {
+        return sdl.c.TTF_FontHeight(self.ptr);
+    }
+
+    pub fn sizeText(self: Font, text: [:0]const u8) !sdl.Size {
+        var w: c_int = undefined;
+        var h: c_int = undefined;
+        if(sdl.c.TTF_SizeText(self.ptr, text, &w, &h) == -1) return makeError();
+        return sdl.Size {.width = w, .height = h};
+    }
+
+    pub fn renderTextSolid(self: Font, text: [:0]const u8, foreground: sdl.Color) !sdl.Surface {
+        return sdl.Surface {
+            .ptr = sdl.c.TTF_RenderText_Solid(
+                self.ptr,
+                text,
+                .{.r=foreground.r, .g=foreground.g, .b=foreground.b, .a=foreground.a}
+            ) orelse return makeError()
+        };
+    }
+
+    pub fn renderTextShaded(
+        self: Font,
+        text: [:0]const u8,
+        foreground: sdl.Color,
+        background: sdl.Color
+    ) !sdl.Surface {
+        return sdl.Surface {
+            .ptr = sdl.c.TTF_RenderText_Shaded(
+                self.ptr,
+                text,
+                .{.r = foreground.r, .g=foreground.g, .b=foreground.b, .a = foreground.a},
+                .{.r = background.r, .g=background.g, .b=background.b, .a = background.a}
+            ) orelse return makeError()
+        };
+    }
+
+    pub fn renderTextBlended(self: Font, text: [:0]const u8, foreground: sdl.Color) !sdl.Surface {
+        return sdl.Surface {
+            .ptr = sdl.c.TTF_RenderText_Blended(
+                self.ptr,
+                text,
+                .{.r=foreground.r, .g=foreground.g, .b=foreground.b, .a=foreground.a}
+            ) orelse return makeError()
+        };
+    }
 
     pub fn close(self: Font) void {
         sdl.c.TTF_CloseFont(self.ptr);
